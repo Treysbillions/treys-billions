c99059abf1f8fa6f919b856ad0b9ea5f136ee91a
Treysbillions <ascompacte10@gmail.com>
2025-01-17 19:44:45 +0400

    maj

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentIndex = 0; // L'index de l'image actuellement affichée
 const slides = document.querySelectorAll('.diaporama-slide');
 const totalSlides = slides.length;
-const slidesPerView = 3; // Nombre d'images visibles en même temps
+const slidesPerView = 1; // Nombre d'images visibles en même temps
 const slideWidth = 100 / slidesPerView; // Largeur de chaque image pour qu'elles s'affichent côte à côte
 
 // Fonction pour passer à la diapositive suivante
